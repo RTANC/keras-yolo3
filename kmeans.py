64dbdc3b3f7fdb277af6b36d582b38d8af58f7f7
RTANC <wongsathon_n@rtanc.ac.th>
2019-10-06 23:05:52 +0700

    fail-07

--- kmeans.py
+++ kmeans.py
@@ -95,7 +95,7 @@ class YOLO_Kmeans:
 
 
 if __name__ == "__main__":
-    cluster_number = 9
+    cluster_number = 9 # 9 = YOLOv3, 6 = YOLOv3-tiny
     filename = "2012_train.txt"
     kmeans = YOLO_Kmeans(cluster_number, filename)
     kmeans.txt2clusters()
